ce47dd52a589ad47ee91545ba94d236fbd32aaa0
Erik <info@zen4all.nl>
2015-11-23 20:09:51 +0100

    initial commit
    Original file set from
    https://www.zen-cart.com/downloads.php?do=file&id=1242

---
+++ includes/languages/english/modules/payment/cop.php
@@ -0,0 +1,19 @@
+<?php
+//
+// +----------------------------------------------------------------------+
+// |zen-cart Open Source E-commerce                                       |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the GPL license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.zen-cart.com/license/2_0.txt.                             |
+// | If you did not receive a copy of the zen-cart license and are unable |
+// | to obtain it through the world-wide-web, please send a note to       |
+// | license@zen-cart.com so we can mail you a copy immediately.          |
+// +----------------------------------------------------------------------+
+// $Id: cop.php 
+//
+
+  define('MODULE_PAYMENT_COP_TEXT_TITLE', 'Cash on Pickup');
+  define('MODULE_PAYMENT_COP_TEXT_DESCRIPTION', 'Cash on Pickup');
+?>
